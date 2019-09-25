6ce41b7f566cb1ab1e8be604f00e7385855e2b91
Mikko Karjalainen <mikko.e.karjalainen@gmail.com>
2019-09-25 11:26:57 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # styx-perf-configs
+
+| Name          | Constraints  | Description |  
+| ------------- | ------------ | ----------- |
+| default       |              | Vanilla application router |
+| default-local |              | Vanilla application router with local origins |
+| pr458         | Requires PR #458 | Origins file backwards compatibility mode |
+| routing       |              | Application router on the routing objects | 
---
+++ default-local/styx-env.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2013-2019 Expedia Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# Styx JVM startup parameters
+#
+# JVM sizing options
+JVM_HEAP_SIZE="${JVM_HEAP_SIZE:=-Xms1536m -Xmx1536m}"
+JVM_HEAP_OPTS="${JVM_HEAP_OPTS:=-XX:+AlwaysPreTouch}"
+JVM_METASPACE="${JVM_METASPACE:=-XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m -XX:MinMetaspaceFreeRatio=0 -XX:MaxMetaspaceFreeRatio=100}"
+JVM_DIRECT_MEMORY="${JVM_DIRECT_MEMORY:=-XX:MaxDirectMemorySize=512m}"
+JVM_OTHER_MEM="${JVM_OTHER_MEM:=-XX:CompressedClassSpaceSize=20m -XX:InitialCodeCacheSize=50m -XX:ReservedCodeCacheSize=50m}"
+
+# Other options
+JVM_HEAP_DUMP="${JVM_HEAP_DUMP:=-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/tmp}"
+
+# Set java flight recorder - Oracle JDK feature
+
+#   517  docker exec -it e88b97226728 sh -c "jcmd 1 JFR.dump filename=/styx/logs/perf-02.jfr"
+
+# Set java VM type
+JVM_FLAVOUR="-server"
+
+JVM_OTHER_OPTS="${JVM_OTHER_OPTS:=-XX:+DisableExplicitGC -Djava.awt.headless=true -XX:+OptimizeStringConcat -XX:+UseBiasedLocking}"
+
+# Increase Netty String builder maximum size to 70KB.
+# See: https://github.com/netty/netty/issues/7092
+JAVA_OPTS="$JAVA_OPTS -Dio.netty.threadLocalMap.stringBuilder.maxSize=71680"
+
+# Set java flight recorder - Oracle JDK feature
+JAVA_OPTS="$JAVA_OPTS -XX:+FlightRecorder"
+
+JAVA_OPTS="$JAVA_OPTS -Xlog:gc*=debug:file=/styx/logs/gc.log"
+JAVA_OPTS="$JAVA_OPTS -DSLIDING_WINDOW_HISTOGRAM_INTERVALS=1 -DINTERVAL_DURATION=6000"
+
+# JVM ROUTE
+APP_jvmRouteName="${APP_jvmRouteName:=}"
+APP_JVM_ROUTE_NAME=$APP_jvmRouteName
+
+export JAVA_OPTS
+
+for var in ${!APP_*}; do
+  export $var
+done
+
+for var in ${!JVM_*}; do
+  export $var
+done
